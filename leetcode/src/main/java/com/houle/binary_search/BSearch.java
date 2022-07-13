108c7713658343a424d6c09e170f010d0b55df62
Houle <houle.cf@foxmail.com>
2022-07-13 22:37:29 +0800

    Java Day Day Up 2022/07/13

---
+++ leetcode/src/main/java/com/houle/binary_search/BSearch.java
@@ -0,0 +1,20 @@
+package com.houle.binary_search;
+
+public class BSearch {
+    public int bsearch(int[] a, int n, int value) {
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high) {
+            int mid = low+((high-low)>>1);
+            if(a[mid] == value) {
+                return mid;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                high = mid -1;
+            }
+        }
+        return -1;
+    }
+}
